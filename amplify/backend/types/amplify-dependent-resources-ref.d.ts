461a2f6ceaf1f0cd86fa4c247a8cd3a52730785b
Jintae Kim <jintaekim2001@gmail.com>
2024-11-28 01:50:42 -0500

    amplify retry

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "storage": {
+    "votingDatabase": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
