63b7921cf0779128ef559d0574e4f6ba0197f476
Jintae Kim <jintaekim2001@gmail.com>
2024-12-03 00:27:14 -0500

    test

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "whatisagame": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "storage": {
     "vote": {
       "Arn": "string",
