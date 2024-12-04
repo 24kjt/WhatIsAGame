9c4549c3a2fade7fb9f03632d4c7cd9501a5e5e6
Jintae Kim <jintaekim2001@gmail.com>
2024-12-04 00:47:02 -0500

    pong scene

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 import Level from "./scenes/Level";
 import Preload from "./scenes/Preload";
+import {VersusGameScene} from "./scenes/VersusGameScene";	
+
 
 class Boot extends Phaser.Scene {
 
@@ -22,15 +24,23 @@ class Boot extends Phaser.Scene {
 window.addEventListener('load', function () {
 	
 	const game = new Phaser.Game({
-		width: 1280,
-		height: 720,
-		backgroundColor: "#2f2f2f",
-		parent: "game-container",
+		type: Phaser.AUTO,
+    width: 800, //prob change to 1280x720
+    height: 600,
+		physics: {
+			default: 'arcade',
+			arcade: {
+				gravity: { y: 0 }, // Set to 0 for a pong-like game
+				debug: false
+			}
+		},
+		
 		scale: {
 			mode: Phaser.Scale.ScaleModes.FIT,
-			autoCenter: Phaser.Scale.Center.CENTER_BOTH
+			autoCenter: Phaser.Scale.Center.CENTER_BOTH,
+			parent: "game-container",
 		},
-		scene: [Boot, Preload, Level]
+		scene: [Boot, Preload, Level, VersusGameScene]
 	});
 
 	game.scene.start("Boot");
